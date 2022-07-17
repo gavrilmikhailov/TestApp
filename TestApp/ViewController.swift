e9c08f4de621e5d5753274983490517afaa9cbb7
Gavril Mikhaylov <gmikay@proton.me>
2022-07-17 22:31:11 +0900

    Removed main storyboard

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
